cc3b0e146d3c08f5811e68146edf9d68e70f123c
Шахмурат <onlymeand@gmail.com>
2024-11-01 22:51:39 +0500

    add fixtures

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,6 +1,19 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 import reverse from '../src/index.js';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+
 test('reverse', () => {
-  expect(reverse('hello')).toEqual('olleh');
+  const text = readFile('reverse_before.txt');
+  const result = readFile('reverse_after.txt');
+
+  expect(reverse(text)).toEqual(result);
   expect(reverse('')).toEqual('');
 });
